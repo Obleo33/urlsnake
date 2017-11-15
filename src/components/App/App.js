ef990258469dc4ecccf88049d7bf671459047d39
Franklin Crosby <obleo33@gmail.com>
2017-11-15 11:52:51 -0700

    Add submiturl component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link, NavLink } from 'react-router-dom'
+
+import SubmitUrl from '../SubmitUrl/SubmitUrl'
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      url: '',
+    }
+  }
+
+  getUrl (url) {
+    this.setState({ url })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to URLSnake</h1>
+        <SubmitUrl getUrl={ this.getUrl.bind(this) }/>
       </div>
     );
   }
---
+++ src/components/SubmitUrl/SubmitUrl.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubmitUrl = (props) => {
+	return(
+		<div>hey</div>
+	)
+}
+
+export default SubmitUrl;
